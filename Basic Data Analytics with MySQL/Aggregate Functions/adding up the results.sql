fe41981d82b9438b1ac52a5add139f94f4ee94e4
BI - Data Analyst <45371372+DataSpieler12345@users.noreply.github.com>
2023-10-19 20:59:39 +0200

    Delete Basic Data Analytics with MySQL/Aggregate Functions directory

--- Basic Data Analytics with MySQL/Aggregate Functions/adding up the results.sql
+++
@@ -1,7 +0,0 @@
-select * from payment
-
-select sum(amount) as 'total_amount' from payment;
-select sum(amount) as 'total_amount', sum(distinct amount) as 'distinct_total_values' from payment;
-select distinct amount from payment;
-
-select staff_id, customer_id, sum(amount) as 'total_amount' from payment group by staff_id, customer_id;
--- Basic Data Analytics with MySQL/Aggregate Functions/finding the average.sql
+++
@@ -1,5 +0,0 @@
-SELECT * FROM sakila.film;
-
-select avg(rental_rate) from film;
-select rating, rental_duration, avg(rental_rate) as 'avg_rental' from film group by rating, rental_duration;
-select rating, rental_duration, avg(rental_rate) as 'avg_rental', avg(length) as 'avg_length' from film group by rating, rental_duration;
